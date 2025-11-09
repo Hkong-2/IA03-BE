81bfeed6db19105977dc8b6f476beb0a1357d8de
Hkong2 <letranhongkonghpt01@gmail.com>
2025-11-09 21:22:41 +0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.enableCors({
+    origin: ['https://ia-03-beta.vercel.app', 'http://localhost:5173'],
+  });
+
+
   await app.listen(process.env.PORT || 5000);
   console.log(`🚀 Server is running on http://localhost:${process.env.PORT || 5000}`);
 }
